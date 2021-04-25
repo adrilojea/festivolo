81b2e9e0ed05f919893c635acda26b059a255b2a
adrilojea <adrilojea114@gmail.com>
2021-04-25 12:17:25 +0200

    Commit inicial del proyecto. Aplicación para la creación, difusión y descubrimientos de eventos musicaleo

---
+++ src/test/java/com/festivolo/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festivolo.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
